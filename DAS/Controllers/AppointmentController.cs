6aff599a0804cd6465eb83d7c5e05303f9d15d6b
Bishwanath Dey Nayan <16103022@iubat.edu>
2019-11-10 23:09:23 +0600

    Payment controller and Views created

--- DAS/Controllers/AppointmentController.cs
+++ DAS/Controllers/AppointmentController.cs
@@ -24,5 +24,52 @@ namespace DAS.Controllers
             }
             return View();
         }
+        public ActionResult ConfirmAppointment(int DoctorId,int ChamberId,int ScheduleId)
+        {
+            AppointmentProperty ap = new AppointmentProperty();
+            ap.DoctorId = DoctorId;
+            ap.ChamberId = ChamberId;
+            ap.ScheduleId = ScheduleId;
+            TempData["AppointmentCredential"] = ap;
+            return View();
+        }
+
+        public ActionResult paymentForm()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult paymentForm(PaymentConfirmationVM pc)
+        {
+            var Data = TempData["AppointmentCredential"] as AppointmentProperty;
+            if (ModelState.IsValid)
+            {
+                Appointment ap = new Appointment()
+                {
+                    P_Name = pc.Name,
+                    Age = pc.Age.ToString(),
+                    Mobile = pc.MobileNo,
+                    Prev_visit = pc.Pre_visited,
+                    DoctorID = Data.DoctorId,
+                    ChamberID = Data.ChamberId,
+                    SechduleId = Data.ScheduleId
+
+                };
+            }
+            return View();
+        }
+
+
+    }
+
+
+    public class AppointmentProperty
+    {
+        public int DoctorId { get; set; }
+        public int ChamberId { get; set; }
+        public int ScheduleId { get; set; }
     }
 }
---
+++ DAS/Models/ViewModel/PaymentConfirmationVM.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DAS.Models.ViewModel
+{
+    public class PaymentConfirmationVM
+    {
+        [Required]
+        [Display(Name="Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name ="Personal Mobile No.")]
+        public string PersonalMobileNo { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name="Email Address")]
+        public string Email { get; set; }
+
+        public int Age { get; set; }
+
+
+        public bool Pre_visited { get; set; }
+        [Required]
+        [Display(Name = "Bkash Mobile No.")]
+        public string MobileNo { get; set; }
+        [Required]
+        [Display(Name ="Transaction Code")]
+        public string Bkash_Tr_Code { get; set; }
+    }
+}
