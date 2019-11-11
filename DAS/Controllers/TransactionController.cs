198216e91f9c1a1f79715dd7f90f8a0dc5844e52
Bishwanath Dey Nayan <16103022@iubat.edu>
2019-11-11 11:54:04 +0600

    Payment confirmation by admin

---
+++ DAS/Controllers/TransactionController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DAS.Controllers
+{
+    public class TransactionController : Controller
+    {
+        private DataBaseContext db = new DataBaseContext();
+        // GET: Transaction
+        public ActionResult PendingTransaction()
+        {
+            var TransactionData = (from a in db.Appointments
+                                   join t in db.TransactionDetails on a.ID equals t.AppointmentId
+                                   where t.isconfirmed == false
+                                   select new DAS.Models.ViewModel.TransactionVM
+                                   {
+
+                                        P_Name = a.P_Name,
+                                        Mobile = a.Mobile,
+                                        Email = a.Email,
+                                        Prev_visit = t.isconfirmed,
+                                        AppointmentId = a.ID,
+                                        BkashMobileNo = t.MobileNo,
+                                        TransactionID = t.Id.ToString(),
+                                        Transactioncode = t.TransactionID,
+                                        date = t.date,
+                                        DoctorID = a.DoctorID,
+                                        ChamberID = a.ChamberID,
+                                        DoctorName = a.Doctor.Name,
+                                        SechduleId = a.SechduleId
+
+                                   }
+                                   ).ToList();
+            return View(TransactionData);
+        }
+
+        public ActionResult ConfirmedTransaction()
+        {
+            var TransactionData = (from a in db.Appointments
+                                   join t in db.TransactionDetails on a.ID equals t.AppointmentId
+                                   where t.isconfirmed == true
+                                   select new DAS.Models.ViewModel.TransactionVM
+                                   {
+
+                                       P_Name = a.P_Name,
+                                       Mobile = a.Mobile,
+                                       Email = a.Email,
+                                       Prev_visit = t.isconfirmed,
+                                       AppointmentId = a.ID,
+                                       BkashMobileNo = t.MobileNo,
+                                       TransactionID = t.Id.ToString(),
+                                       Transactioncode = t.TransactionID,
+                                       date = t.date,
+                                       DoctorID = a.DoctorID,
+                                       ChamberID = a.ChamberID,
+                                       DoctorName = a.Doctor.Name,
+                                       SechduleId = a.SechduleId
+
+                                   }
+                                ).ToList();
+            return View(TransactionData);
+        }
+    }
+}
---
+++ DAS/Models/ViewModel/TransactionVM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DAS.Models.ViewModel
+{
+    public class TransactionVM
+    {
+        public int AppointmentId { get; set; }
+        public string Mobile { get; set; }
+        public string TransactionID { get; set; }
+        public bool isconfirmed { get; set; }
+        public DateTime date { get; set; }
+        public string P_Name { get; set; }
+        public string Age { get; set; }
+        public string BkashMobileNo { get; set; }
+        public string Email { get; set; }
+        public bool Prev_visit { get; set; }
+        public int DoctorID { get; set; }
+        public int ChamberID { get; set; }
+        public int SechduleId { get; set; }
+        public string DoctorName { get; set; }
+        public string Transactioncode { get; set; }
+    }
+}
